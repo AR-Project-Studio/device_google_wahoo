7c0bf4d5e8f4ad769173efb017b9c847d98e1047
Nathan <nathan@lunarixus.dev>
2025-01-27 00:42:42 +0800

    wahoo: Remove packages at the build time

---
+++ RemovePkgs/Android.mk
@@ -0,0 +1,16 @@
+OCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+          Drive \
+          Maps \
+          Music \
+          PrebuiltGmail \
+          YouTube \
+          YouTubeMusicPrebuilt 
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
--- device.mk
+++ device.mk
@@ -415,6 +415,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ElmyraService
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+     RemovePkgs
+
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
